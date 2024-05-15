dbcd5f5531d82d6349bfd8800ad7b9e6dd320598
Flavio-Converso <fla.converso@gmail.com>
2024-05-15 15:48:50 +0200

    .

--- media player test/sidebar.js
+++ media player test/sidebar.js
@@ -29,10 +29,12 @@ function store(dati) {
     const albumTitle = element.album.title;
     const artistName = element.artist.name;
     const albumCover = element.album.cover_medium;
+    const previewUrl = element.preview;
     const album = {
       albumTitle: albumTitle,
       artistName: artistName,
       albumCover: albumCover,
+      preview: previewUrl,
     };
 
     albums.push(album);
@@ -101,13 +103,13 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
   }
+  localStorage.setItem("searchResult", JSON.stringify(albums));
   console.log(albums);
 
-  for (let i = 1; i <= 10; i++) {
+  for (let i = 1; i <= 20; i++) {
     let card = document.createElement("div");
     card.classList.add("col-12", "col-sm-6", "col-lg-3", "col-xl-4", "mt-3");
     card.innerHTML = `
-              <a href="#" class="text-decoration-none">
                   <div class="card mb-3 grey-horizontal-card position-relative ">
                       <div class="row ">
                           <div class="col d-flex align-items-center">
@@ -124,12 +126,369 @@ document.addEventListener("DOMContentLoaded", function () {
                           </div>
                       </div>
                       <div class="play-badge">
-                          <img class="playButton" src="/assets/imgs/svg/play-fill.svg"
-                              alt="play button" />
+                          <button class="bg-transparent border-0 play-button-card" data-index="${i}><img class="playButton" src="../assets/imgs/svg/play-fill.svg"
+                              alt="play button" /></button>
                       </div>
                   </div>
-              </a>
+              
               </div>`;
     document.getElementById("printHorizontalCards").appendChild(card);
   }
 });
+
+
+
+
+
+
+// MEDIA PLAYER
+document.addEventListener("DOMContentLoaded", function () {
+  // Seleziona gli elementi HTML necessari
+  const playPauseButton = document.querySelector(".play-pause");
+  const progressBar = document.querySelector(".progress");
+  const progressReference = document.querySelector(".progress-reference");
+  const currentTimeDisplay = document.querySelector(".current-time");
+  const volumeBar = document.querySelector(".volume-bar .progress");
+  const volumeIcon = document.querySelector(".volume-bar i");
+  const volumeReference = document.querySelector(".volume-reference");
+  const randomButton = document.querySelector(".fa-random");
+  const heartButton = document.querySelector(".far.fa-heart");
+  const rewindButton = document.querySelector("#rewindBtn");
+  const audioElement = document.querySelector("audio");
+
+  // Variabili di stato per il player audio
+  let isPlaying = false;
+  let isMuted = false;
+  let isLiked = false;
+  let isRandom = false;
+  progressBar.style.width = "0%";
+
+  // Funzione per gestire il click sulla progress bar
+  function setProgress(e) {
+    const progressReferenceWidth = progressReference.clientWidth;
+    const clickX = e.clientX - progressReference.getBoundingClientRect().left;
+    const relativeClickX = Math.min(1, Math.max(0, clickX / progressReferenceWidth));
+
+    // Aggiorna la posizione della progress bar
+    progressBar.style.width = `${relativeClickX * 100}%`;
+
+    // Aggiorna il tempo di riproduzione dell'audio
+    const duration = audioElement.duration;
+    audioElement.currentTime = duration * relativeClickX;
+  }
+
+  // Aggiungi un listener per l'evento mousedown sulla barra di riferimento
+  progressReference.addEventListener("mousedown", startDrag);
+
+  // Funzione per aggiornare la posizione della progress bar durante il trascinamento
+  function updateProgressBarDuringDrag(e) {
+    if (isDragging) {
+      setProgress(e);
+    }
+  }
+
+  // Definisce una funzione per gestire l'inizio del trascinamento
+  function startDrag(e) {
+    isDragging = true;
+    updateProgressBarDuringDrag(e);
+    // Aggiungi listener per mousemove e mouseup sul documento
+    document.addEventListener("mousemove", updateProgressBarDuringDrag);
+    document.addEventListener("mouseup", stopDrag);
+  }
+
+  // Definisce una funzione per gestire la fine del trascinamento
+  function stopDrag() {
+    isDragging = false;
+    // Rimuovi i listener per mousemove e mouseup sul documento
+    document.removeEventListener("mousemove", updateProgressBarDuringDrag);
+    document.removeEventListener("mouseup", stopDrag);
+  }
+
+  audioElement.addEventListener("timeupdate", updateProgressBar);
+
+  // Funzione per aggiornare la progress bar in base alla riproduzione dell'audio
+  function updateProgressBar() {
+    const currentTime = audioElement.currentTime;
+    const duration = audioElement.duration;
+    const progress = (currentTime / duration) * 100;
+
+    // Aggiorna la larghezza della progress bar
+    progressBar.style.width = `${progress}%`;
+
+    // Aggiorna il tempo corrente di riproduzione
+    currentTimeDisplay.textContent = formatTime(currentTime);
+  }
+
+  // Aggiungi gli eventi ai pulsanti e alle barre
+  playPauseButton.addEventListener("click", togglePlayPause);
+  volumeIcon.addEventListener("click", toggleMute);
+  randomButton.addEventListener("click", toggleRandom);
+  heartButton.addEventListener("click", toggleHeart);
+  rewindButton.addEventListener("click", rewind);
+  volumeReference.addEventListener("click", setVolume);
+
+  // Funzione per gestire il play e il pausa
+  function togglePlayPause() {
+    isPlaying = !isPlaying;
+    if (isPlaying) {
+      audioElement.play();
+      playPauseButton.classList.remove("fa-play");
+      playPauseButton.classList.add("fa-pause");
+    } else {
+      audioElement.pause();
+      playPauseButton.classList.remove("fa-pause");
+      playPauseButton.classList.add("fa-play");
+    }
+  }
+
+  // Funzione per gestire il mute
+  function toggleMute() {
+    isMuted = !isMuted;
+    if (isMuted) {
+      // Memorizza temporaneamente il volume prima di impostarlo a zero
+      localStorage.setItem("previousVolume", audioElement.volume);
+
+      audioElement.volume = 0;
+      volumeIcon.classList.remove("fa-volume-down");
+      volumeIcon.classList.add("fa-volume-mute", "active");
+      volumeBar.style.width = "0%"; // Imposta la barra del volume a zero
+    } else {
+      // Ripristina il volume al valore memorizzato
+      const previousVolume = localStorage.getItem("previousVolume");
+      audioElement.volume = previousVolume ? parseFloat(previousVolume) : 1;
+
+      volumeIcon.classList.remove("fa-volume-mute", "active");
+      volumeIcon.classList.add("fa-volume-down");
+
+      // Aggiorna la larghezza della barra del volume
+      volumeBar.style.width = `${audioElement.volume * 100}%`;
+    }
+  }
+
+  // Funzione per gestire la riproduzione casuale
+  function toggleRandom() {
+    isRandom = !isRandom;
+    if (isRandom) {
+      randomButton.classList.add("active");
+      // Logica per riproduzione casuale
+    } else {
+      randomButton.classList.remove("active");
+      // Logica per riproduzione normale
+    }
+  }
+
+  // Funzione per gestire il like
+  function toggleHeart() {
+    isLiked = !isLiked;
+    if (isLiked) {
+      heartButton.classList.remove("far");
+      heartButton.classList.add("fas", "active");
+      // Logica per like
+    } else {
+      heartButton.classList.remove("fas", "active");
+      heartButton.classList.add("far");
+      // Logica per rimuovere il like
+    }
+  }
+
+  // Funzione per riprodurre (da implementare)
+  function rewind() {
+    // Logica per riprodurre (se vogliamo implementarla)
+    isRandom = !isRandom;
+    if (isRandom) {
+      rewindButton.classList.add("active");
+      // Logica per riproduzione casuale
+    } else {
+      rewindButton.classList.remove("active");
+      // Logica per riproduzione normale
+    }
+  }
+
+  // Funzione per impostare il volume
+  function setVolume(e) {
+    const volumeReferenceWidth = volumeReference.clientWidth;
+    const clickX = e.clientX - volumeReference.getBoundingClientRect().left;
+    const relativeClickX = Math.min(1, Math.max(0, clickX / volumeReferenceWidth));
+
+    // Aggiorna la posizione della barra del volume
+    volumeBar.style.width = `${relativeClickX * 100}%`;
+
+    // Aggiorna il volume dell'audio
+    audioElement.volume = relativeClickX;
+  }
+
+  // Aggiorna il tempo corrente e totale ogni secondo
+  setInterval(updateTimeDisplays, 1000);
+
+  function updateTimeDisplays() {
+    const currentTimeDisplay = document.querySelector(".current-time");
+    const totalTimeDisplay = document.querySelector(".total-time");
+
+    if (audioElement && !isNaN(audioElement.duration)) {
+      const currentTime = audioElement.currentTime;
+      const totalTime = audioElement.duration;
+      const formattedCurrentTime = formatTime(currentTime);
+      const formattedTotalTime = formatTime(totalTime);
+
+      currentTimeDisplay.textContent = formattedCurrentTime;
+      totalTimeDisplay.textContent = formattedTotalTime;
+    } else {
+      // Se non c'è un elemento audio o se la durata non è un numero, mostra "--.--"
+      currentTimeDisplay.textContent = "--:--";
+      totalTimeDisplay.textContent = "--:--";
+    }
+  }
+
+  // Funzione per formattare il tempo in mm:ss
+  function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    seconds = Math.floor(seconds % 60);
+    return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  }
+
+  // Definisci una funzione per rimuovere l'elemento 'divider' quando la larghezza supera i 768px
+  function removeDividerOnResize() {
+    if (window.innerWidth >= 768) {
+      var elementsToRemove = document.getElementsByClassName('divider');
+      for (var i = 0; i < elementsToRemove.length; i++) {
+        var element = elementsToRemove[i];
+        if (element && element.parentNode) {
+          element.parentNode.removeChild(element);
+        }
+      }
+    }
+  }
+
+
+  // Esegui la funzione all'avvio e ogni volta che la finestra viene ridimensionata
+  removeDividerOnResize(); // Esegui subito al caricamento della pagina
+  window.addEventListener('resize', removeDividerOnResize); // Esegui ogni volta che la finestra viene ridimensionata
+
+  // Definisci una funzione per gestire l'inizio del trascinamento
+  function startDrag(e) {
+    isDragging = true;
+    updateProgressBarDuringDrag(e);
+    // Aggiungi listener per mousemove e mouseup sul documento
+    document.addEventListener("mousemove", updateProgressBarDuringDrag);
+    document.addEventListener("mouseup", stopDrag);
+  }
+
+  // Funzione per aggiornare la posizione della progress bar durante il trascinamento
+  function updateProgressBarDuringDrag(e) {
+    if (isDragging) {
+      const progressReferenceWidth = progressReference.clientWidth;
+      const clickX = e.clientX - progressReference.getBoundingClientRect().left;
+      const relativeClickX = Math.min(1, Math.max(0, clickX / progressReferenceWidth));
+
+      // Aggiorna la posizione della progress bar
+      progressBar.style.width = `${relativeClickX * 100}%`;
+
+      // Aggiorna il tempo di riproduzione dell'audio
+      const duration = audioElement.duration;
+      audioElement.currentTime = duration * relativeClickX;
+    }
+  }
+
+});
+
+
+// STAMPA NEL MEDIA PLAYER
+document.addEventListener("DOMContentLoaded", function () {
+  // Seleziona il bottone playButtonMain
+  const playButtonMain = document.getElementById("playButtonMain");
+
+  // Aggiungi un listener per il click sul bottone playButtonMain
+  playButtonMain.addEventListener("click", handlePlayButtonClick);
+  const mediaPlayerImage = document.getElementById("media-image");
+  const divider = document.getElementById("divisore")
+  // Funzione per gestire il click su playButtonMain
+  function handlePlayButtonClick() {
+    // Seleziona gli elementi HTML necessari per visualizzare i dettagli dell'album nel media player
+    const albumImage = document.getElementById("media-image");
+    const albumTitle = document.querySelector("p.title");
+    const artistName = document.querySelector("p.artist");
+
+    // Ottieni i dati dell'album dal localStorage
+    const storedData = localStorage.getItem("searchResult");
+    const album = JSON.parse(storedData);
+
+    // Assicurati che ci siano album e che ne esista almeno uno
+    if (album && album.length > 0) {
+      // Ottieni il primo album (puoi modificare questa logica per ottenere l'album desiderato)
+      const firstAlbum = album[0];
+
+      // Imposta i valori degli elementi HTML con i dati dell'album
+      albumImage.src = firstAlbum.albumCover; // Imposta l'immagine dell'album
+      albumTitle.textContent = firstAlbum.albumTitle; // Imposta il titolo dell'album
+      artistName.textContent = firstAlbum.artistName; // Imposta il nome dell'artista
+      const audioPlayer = document.getElementById("audio-player");
+      if (firstAlbum.preview) {
+        mediaPlayerImage.style.display = "block";
+        divider.style.display = "block";
+        const playPauseButton = document.querySelector(".play-pause");
+        playPauseButton.classList.remove("fa-play");
+        playPauseButton.classList.add("fa-pause");
+        audioPlayer.src = firstAlbum.preview;
+        audioPlayer.play(); // Avvia la riproduzione automaticamente
+      } else {
+        console.error("L'album non ha un'anteprima audio.");
+      }
+    }
+  }
+  const playButtons = document.querySelectorAll(".play-button-card");
+
+  // Aggiungi un listener per il click su ogni bottone play-button-card
+  playButtons.forEach(function (playButton) {
+    playButton.addEventListener("click", handlePlayButtonCardClick);
+  });
+
+  // Funzione per gestire il click su ogni bottone play-button-card
+  function handlePlayButtonCardClick(event) {
+    // Ottieni l'indice della card dalla proprietà data-index del bottone cliccato
+    let index = parseInt(event.currentTarget.getAttribute("data-index"), 10);
+    console.log("Clicked index:", index);
+
+
+    // Ottieni i dati dell'album dal localStorage
+    const storedData = localStorage.getItem("searchResult");
+    const albums = JSON.parse(storedData);
+    // Assicurati che ci siano album e che l'indice sia valido
+    if (albums && albums.length > index && index >= 0) {
+      // Ottieni i dati dell'album relativo all'indice cliccato
+      const album = albums[index];
+      // Trasferisci i valori nella sezione del media player
+
+      if (!mediaPlayerImage) {
+        console.error("Elemento mediaPlayerImage non trovato.");
+        return;
+      }
+      const mediaPlayerTitle = document.querySelector("p.title");
+      const mediaPlayerArtist = document.querySelector("p.artist");
+      console.log("Albums:", albums);
+      console.log("Albums length:", albums.length);
+      console.log("Index valid:", albums && albums.length > index && index >= 0);
+      const audioPlayer = document.getElementById("audio-player");
+      mediaPlayerImage.src = album.albumCover;
+      if (!album.albumCover) {
+        console.error("album.albumCover non è valido:", album.albumCover);
+        return;
+      }
+      mediaPlayerTitle.textContent = album.albumTitle;
+      mediaPlayerArtist.textContent = album.artistName;
+      if (album.preview) {
+        divider.style.display = "block";
+        mediaPlayerImage.style.display = "block";
+        const playPauseButton = document.querySelector(".play-pause");
+        playPauseButton.classList.remove("fa-play");
+        playPauseButton.classList.add("fa-pause");
+        audioPlayer.src = album.preview;
+        audioPlayer.play(); // Avvia la riproduzione automaticamente
+      } else {
+        console.error("L'album non ha un'anteprima audio.");
+      }
+    }
+  }
+});
+
+
+
